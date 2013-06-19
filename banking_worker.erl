efa4a43253f2def5c2527bf8a74361c08d29d2d3
Alexander Baumgärtner <baumgaertner.al@gmail.com>
2013-06-19 12:55:25 +0200

    dets aktualisiert

--- banking_worker.erl
+++ banking_worker.erl
@@ -3,43 +3,48 @@
 -export([konto_anlegen/1]).
 
 
+error_handling({error, Reason}) ->
+	io:format("Error: ~p ~n", [Reason]),
+	dets:close(konten),
+	exit(Reason)
+	.
 	
-db_oeffnen(error, Reason) ->
-	io:format("Error: ~p", [Reason]).
-
-
-Daten_lesen(Kontonr) ->
+	
+daten_lesen(Kontonr) ->
+	{Response, Reason} = dets:open_file(konten, [{file, "db_konten"}, {type, set}]),
+	error_handling(Response, Reason),
+	Konto = dets:lookup(konten, Kontonr),
+	error_handling(Konto),
+	dets:close(konten)
+	.
+	
+	
+daten_schreiben(konto_anlegen) -> 
 	{Response, Reason} = dets:open_file(konten, [{file, "db_konten"}, {type, set}]),
 	db_oeffnen(Response, Reason),
-	Konto = dets:lookup(konten, Kontonr).
-	
-	
-	
-	
-	
-	
-Daten_schreiben(konto_anlegen) -> 
-	{Response, Reason} = dets:open_file(konten, [{file, "db_konten"}, {type, set}]),
-	db_oeffnen(Response, Reason),
-	Kontonr_temp = first(konten),
+	Kontonr_temp = dets:first(konten),
 	Konten_init(Kontonr_tmp), %Für den fall dass noch keine Konten existieren
-	{0, HoechsteKontonr} = lookup(konten, 0),
+	{0, HoechsteKontonr} = dets:lookup(konten, 0),
 	NeueKontonr = HoechsteKontonr + 1,
-	insert(konten, {NeueKontonr, 
+	dets:insert(konten, {NeueKontonr, 
 								{sperrvermerk, false},
 								{vermoegen, 0},
 								{maxDispo, 0},
 								{dispoZins, 0},
 								{transaktionsliste, []}}),
-								
+	dets:close(konten),
+	NeueKontonr;
 	
-	
-	
-Konten_init('$end_of_table') ->
-	insert(konten, {0, 0});
-Konten_init(_) -> .
+daten_schreiben(Konto) -> 
+	dets:open_file(konten, [{file, "db_konten"}, {type, set}]),
+	dets:insert(konten, Konto),
+	dets:close(konten).
 
-	Kontonr = first(konten),
+konten_init('$end_of_table') ->
+	dets:insert(konten, {0, 0});
+konten_init(_) -> .
+
+	Kontonr = dets:first(konten),
 	get_next_konto(Kontonr, 0)
 	
 get_next_konto('$end_of_table', Letzte_kontonr) ->
@@ -50,11 +55,6 @@ get_next_konto(AlteKontonr, _) ->
 	NeueKontonr = next(konten, AlteKontonr),
 	get_next_konto(NeueKontonr, AlteKontonr).
 	
-
-
-Daten_schreiben(Konto) -> 
-	dets:open_file(konten, [{file, "db_konten"}, {type, set}])
-
 
 konto_anlegen(PID) ->
 	
