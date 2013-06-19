21262c13f81d1ba33f01310fd1b24dbc41db2f36
Alexander Baumgärtner <baumgaertner.al@gmail.com>
2013-06-19 15:40:45 +0200

    konto anlegen funktioniert

--- banking_worker.erl
+++ banking_worker.erl
@@ -8,61 +8,59 @@ error_handling({error, Reason}) ->
 	dets:close(konten),
 	exit(Reason)
 	.
+
 	
+
+		
+get_next_konto('$end_of_table', Letzte_kontonr) ->
+	Letzte_kontonr;
+get_next_konto(AlteKontonr, _) -> 
+	NeueKontonr = dets:next(konten, AlteKontonr),
+	get_next_konto(NeueKontonr, AlteKontonr).
 	
+
 daten_lesen(Kontonr) ->
 	{Response, Reason} = dets:open_file(konten, [{file, "db_konten"}, {type, set}]),
-	error_handling(Response, Reason),
+	error_handling({Response, Reason}),
 	Konto = dets:lookup(konten, Kontonr),
-	error_handling(Konto),
+	case Konto = {error, Reason} of
+		true -> 
+				error_handling(Konto)
+	end,
 	dets:close(konten)
 	.
 	
-	
+
 daten_schreiben(konto_anlegen) -> 
 	{Response, Reason} = dets:open_file(konten, [{file, "db_konten"}, {type, set}]),
-	db_oeffnen(Response, Reason),
 	Kontonr_temp = dets:first(konten),
-	Konten_init(Kontonr_tmp), %Für den fall dass noch keine Konten existieren
-	{0, HoechsteKontonr} = dets:lookup(konten, 0),
+	io:format("vor case~n"),
+	case Kontonr_temp of 		%%Konteninit
+		'$end_of_table' -> dets:insert(konten, {0, 0});
+		_ -> ignoreit
+	end,
+	io:format("nach case~n"),
+	[{0, HoechsteKontonr}] = dets:lookup(konten, 0),
 	NeueKontonr = HoechsteKontonr + 1,
+	io:format("vor insert~n"),
+	io:format("neuekontonr: ~p ~n", [NeueKontonr]),
 	dets:insert(konten, {NeueKontonr, 
 								{sperrvermerk, false},
 								{vermoegen, 0},
 								{maxDispo, 0},
 								{dispoZins, 0},
 								{transaktionsliste, []}}),
+	dets:insert(konten, {0, NeueKontonr}),
 	dets:close(konten),
+	io:format("nach close~n"),
 	NeueKontonr;
-	
 daten_schreiben(Konto) -> 
 	dets:open_file(konten, [{file, "db_konten"}, {type, set}]),
 	dets:insert(konten, Konto),
-	dets:close(konten).
-
-konten_init('$end_of_table') ->
-	dets:insert(konten, {0, 0});
-	
-konten_init(_) -> .
-
-%	Kontonr = dets:first(konten),
-%	get_next_konto(Kontonr, 0)
-	
-get_next_konto('$end_of_table', Letzte_kontonr) ->
-	
-
-
-get_next_konto(AlteKontonr, _) -> 
-	NeueKontonr = next(konten, AlteKontonr),
-	get_next_konto(NeueKontonr, AlteKontonr).
-	
+	dets:close(konten).	
 
 konto_anlegen(PID) ->
-	
-   % TODO: Kontonummer ist der Key
-   PID ! {ok, "12345"},
-   receive
-      abc -> x
-   end.
+	Neue_kontonr = daten_schreiben(konto_anlegen),
+    PID ! {ok, Neue_kontonr}.
    
   
